eb4be6f2515f90ee2d42dcdd73d1c7a870288b67
M Arya Anggara Gustiana <aryagustiana101@gmail.com>
2022-08-25 00:22:50 +0700

    chore: prisma setup

---
+++ prisma/migrations/20220824170119_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Acivity` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/controllers/activity.ts
+++ src/controllers/activity.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import prisma from '../libs/prisma'
 
 export const getMultiple = async (req: Request, res: Response): Promise<Response> => {
+  
+  await prisma.acivity.create({
+    data: {
+      title: 'test',
+      email: 'asd'
+    },
+  })
+
   return res.status(200).json({
-    message: 'Activity registered'
+    activities: await prisma.acivity.findMany()
   })
 }
