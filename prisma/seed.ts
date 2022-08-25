4f99e86c296e8481fc4ea98fc0a9ab3d84923ae6
M Arya Anggara Gustiana <aryagustiana101@gmail.com>
2022-08-26 03:33:17 +0700

    fix: liltle tweak for docker

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,25 +4,27 @@ import prisma from '../src/libs/prisma'
 faker.setLocale('id_ID')
 
 const seed = async (): Promise<void> => {
-  const priority = ['very-high', 'high', 'medium', 'low', 'very-low']
+  if ((await prisma.activity.count()) < 1 || (await prisma.todo.count()) < 1) {
+    const priority = ['very-high', 'high', 'medium', 'low', 'very-low']
 
-  for (let i = 0; i < 5; i++) {
-    const activity = await prisma.activity.create({
-      data: {
-        title: faker.lorem.sentence(3),
-        email: faker.internet.email().toLowerCase()
-      }
-    })
-
-    for (let j = 0; j < 20; j++) {
-      await prisma.todo.create({
+    for (let i = 0; i < 5; i++) {
+      const activity = await prisma.activity.create({
         data: {
-          title: faker.lorem.sentence(5),
-          is_active: Math.random() >= 0.5,
-          priority: priority[Math.floor(Math.random() * priority.length)],
-          activity: { connect: { id: activity.id } }
+          title: faker.lorem.sentence(3),
+          email: faker.internet.email().toLowerCase()
         }
       })
+
+      for (let j = 0; j < 20; j++) {
+        await prisma.todo.create({
+          data: {
+            title: faker.lorem.sentence(5),
+            is_active: Math.random() >= 0.5,
+            priority: priority[Math.floor(Math.random() * priority.length)],
+            activity: { connect: { id: activity.id } }
+          }
+        })
+      }
     }
   }
 }
