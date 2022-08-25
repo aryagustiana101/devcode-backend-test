ba6451d9083fa5f9dd1ed89b586842dd435e8df9
M Arya Anggara Gustiana <aryagustiana101@gmail.com>
2022-08-25 12:18:01 +0700

    chore: prisma setup fix

--- src/controllers/activity.ts
+++ src/controllers/activity.ts
@@ -2,7 +2,7 @@ import prisma from '../libs/prisma'
 import { Request, Response } from 'express'
 
 export const getMultiple = async (req: Request, res: Response): Promise<Response> => {
-  await prisma.acivity.create({
+  await prisma.activity.create({
     data: {
       title: 'test',
       email: 'asd'
@@ -10,6 +10,6 @@ export const getMultiple = async (req: Request, res: Response): Promise<Response
   })
 
   return res.status(200).json({
-    activities: await prisma.acivity.findMany()
+    activities: await prisma.activity.findMany()
   })
 }
