cc654876f484e3f84b75e0cb55c7bb6e1490af5b
M Arya Anggara Gustiana <aryagustiana101@gmail.com>
2022-08-28 19:24:21 +0700

    fix: morgan log for dev only

--- app.ts
+++ app.ts
@@ -10,10 +10,13 @@ const app: Express = express()
 
 app.use(cors())
 app.use(compression())
-app.use(logger('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'))
+}
 
 app.use(index)
 
